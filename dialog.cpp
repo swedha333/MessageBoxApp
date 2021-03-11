8648417c0043066c6e687ac19f675273d613082f
abinaya717 <80210555+abinaya717@users.noreply.github.com>
2021-03-11 10:41:32 +0530

    Add files via upload

---
+++ dialog.cpp
@@ -0,0 +1,59 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include<QMessageBox>
+#include<QDebug>
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_pushButton_clicked()
+{
+    QMessageBox::information(this,"Abinaya 212218104005","THIS IS INFORMATION BOX");
+}
+
+void Dialog::on_pushButton_2_clicked()
+{
+    QMessageBox::StandardButton reply=QMessageBox::question(this,"Abinaya 212218104005","Do you like apples?",QMessageBox::Yes | QMessageBox::No);
+    if(reply == QMessageBox::Yes)
+    {
+        QMessageBox::information(this,"APPLE","Yes");
+    }
+    if(reply == QMessageBox::No)
+    {
+        QMessageBox::information(this,"NO APPLE","No");
+    }
+
+}
+
+void Dialog::on_pushButton_3_clicked()
+{
+    QMessageBox::StandardButton reply=QMessageBox::question(this,"Abinaya 212218104005","This is a custom message",QMessageBox::Yes | QMessageBox::YesToAll | QMessageBox::No | QMessageBox::NoToAll);
+    if(reply == QMessageBox::Yes)
+    {
+        qDebug() <<"Nothing is displayed";
+    }
+    if(reply == QMessageBox::YesToAll)
+    {
+       QMessageBox::warning(this,"Abinaya 212218104005","Display the message with warning as YesToAll");
+    }
+    if(reply == QMessageBox::NoToAll)
+    {
+       QMessageBox::warning(this,"Abinaya 212218104005","Don't Display the message with warning as YesToAll");
+    }
+
+
+}
+
+void Dialog::on_pushButton_4_clicked()
+{
+    QMessageBox::warning(this,"Abinaya 212218104005","This is a warning message");
+}
